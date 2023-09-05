90895b93c7348025632de3cd16f2ab6caaf6a24d
ducvinh-dev <ldvinh596@gmail.com>
2023-09-05 23:04:25 +0700

    init commit

---
+++ starter-template/src/types/utils.ts
@@ -0,0 +1,8 @@
+import { useSearchParams } from 'react-router-dom'
+
+export const useQueryString = () => {
+  const [searchParams] = useSearchParams()
+  const searchParamsObj = Object.fromEntries([...searchParams])
+
+  return searchParamsObj
+}
